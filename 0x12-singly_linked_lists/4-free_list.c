e733c6da79831cecb4073342e15bb21617fd06da
6godpro <erickson5115@gmail.com>
2022-11-24 12:52:25 -0800

    completed task 4

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ * free_list - frees a list_t list.
+ * @head: list to be freed.
+ * 
+ * Return: void.
+ */
+void free_list(list_t *head)
+{
+	if (head == NULL)
+		return;
+	else
+	{
+		while (head != NULL)
+		{
+			free(head->str);
+			free(head);
+			head = head->next;
+		}
+	}
+}
