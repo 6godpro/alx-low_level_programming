7da5ee8473c274e96f98bfb583b3cbba247af7e0
6godpro <erickson5115@gmail.com>
2022-11-24 13:32:42 -0800

    fixed trailing space and excess description

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,13 +1,12 @@
-#include "lists.h"
 #include <stdio.h>
 
 /**
  * first - prints a string before the main method executes.
- * @str: string to be printed.
  */
 void first(void) __attribute__ ((constructor));
 
 void first(void)
 {
-	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,\n
+		I bore my house upon my back!\n");
 }
--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -4,24 +4,20 @@
 /**
  * free_list - frees a list_t list.
  * @head: list to be freed.
- * 
+ *
  * Return: void.
  */
 void free_list(list_t *head)
 {
-	list_t *tmp;
 
 	if (head == NULL)
 		return;
-	else
+
+	while (head != NULL)
 	{
-		while (head != NULL)
-		{
-			tmp = head;
-			free(head->str);
-			free(head);
-			head = head->next;
-			head = tmp;
-		}
+		free(head->str);
+		free(head);
+		head = head->next;
 	}
+
 }
