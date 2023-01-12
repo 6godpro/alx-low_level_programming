af5b005ea45482a938f9d664a26d0418da038ea3
6godpro <erickson5115@gmail.com>
2023-01-12 12:15:10 -0800

    completed task 102 + optimized task 8

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -16,25 +16,24 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 	if (*head == NULL)
 		return (-1);
 
-	if (index == 0)
-	{
-		*head = tmp->next;
-		if (*head)
-			(*head)->prev = NULL;
-		free(tmp);
-		return (1);
-	}
 	for (; index != 0; index--)
 	{
 		if (tmp == NULL)
 			return (-1);
 		tmp = tmp->next;
 	}
-	(tmp->prev)->next = tmp->next;
-
-	if (tmp->next)
-		(tmp->next)->prev = tmp->prev;
-
+	if (tmp == *head)
+	{
+		*head = tmp->next;
+		if (*head)
+			(*head)->prev = NULL;
+	}
+	else
+	{
+		(tmp->prev)->next = tmp->next;
+		if (tmp->next)
+			(tmp->next)->prev = tmp->prev;
+	}
 	free(tmp);
 	return (1);
 }
