c14fbc8cb6b26c7b7c70138e335a9121e1ee9bfd
6godpro <erickson5115@gmail.com>
2023-02-04 01:59:40 -0800

    minor fix

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -1,6 +1,5 @@
 #include "hash_tables.h"
 
-
 void hash_node_delete(hash_node_t *node);
 
 /**
@@ -10,11 +9,14 @@ void hash_node_delete(hash_node_t *node);
 void hash_table_delete(hash_table_t *ht)
 {
 	unsigned long int i;
+
+	if (ht == NULL)
+		return;
+
 	for (i = 0; i < ht->size; i++)
 	{
-		if (ht->array[i] == NULL)
-			continue;
-		hash_node_delete(ht->array[i]);
+		if (ht->array[i] != NULL)
+			hash_node_delete(ht->array[i]);
 	}
 	free(ht->array);
 	free(ht);
