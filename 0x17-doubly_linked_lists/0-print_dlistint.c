80424f7ac57e70aa8e9a942f8c3be413fe5a10a1
6godpro <erickson5115@gmail.com>
2023-03-16 11:51:25 -0700

    fixed indents

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -8,17 +8,16 @@
  */
 size_t print_dlistint(const dlistint_t *h)
 {
-        size_t nodes = 0;
+	size_t nodes = 0;
 
-        if (h == NULL)
-                return (0);
+	if (h == NULL)
+		return (0);
 
-        while (h)
-        {
-                printf("%d\n", h->n);
-                nodes++;
-                h = h->next;
-        }
-
-        return (nodes);
+	while (h)
+	{
+		printf("%d\n", h->n);
+		nodes++;
+		h = h->next;
+	}
+	return (nodes);
 }
