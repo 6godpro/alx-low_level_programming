63768f81501792bbd6bce1559f70c3b00fd9bfd5
6godpro <erickson5115@gmail.com>
2023-05-24 21:11:54 +0100

    (fix)-> changed conditional

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -13,22 +13,24 @@ def island_perimeter(grid):
     Return: The perimeter of the island."""
 
     perimeter = 0
+    col = len(grid)
+    row = len(grid[0])
 
-    for i in range(len(grid)):
-        for j in range(len(grid[0])):
+    for i in range(col):
+        for j in range(row):
             if grid[i][j] == 1:
                 perimeter += 4
 
                 if i != 0 and grid[i - 1][j] == 1:
                     perimeter -= 1
 
-                if grid[i + 1][j] == 1:
+                if i < col - 1 and grid[i + 1][j] == 1:
                     perimeter -= 1
 
                 if j != 0 and grid[i][j - 1] == 1:
                     perimeter -= 1
 
-                if grid[i][j + 1] == 1:
+                if j < row - 1 and grid[i][j + 1] == 1:
                     perimeter -= 1
 
     return perimeter
